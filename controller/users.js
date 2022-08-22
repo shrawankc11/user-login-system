0a363f1bd20915c703a952697bfe4ffdf40b2e57
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-08-22 22:09:42 +0545

    added refresh tokens to seperate user database

--- controller/users.js
+++ controller/users.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 //define refresh Token to store all users refresh tokens here
 //this will be empty when server restarts since we do not have any state
-let refreshTokens = []
+// let refreshTokens = []
 
 userRouter.get('/', async (req, res) => {
     const users = await User.find({}).populate('notes', { note: 1 })
@@ -20,23 +20,27 @@ userRouter.get('/:id', async (req, res) => {
 
 //this route will remove the refresh token that was received from a specific user
 //user is unable to make requests to specific endpoint after this action
-userRouter.post('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    return res.status(204).json({ message: "succesfully logged out" })
+userRouter.post('/logout', async (req, res) => {
+    const user = await User.findById(req.body.id)
+    user.refreshToken = undefined
+    await user.save()
+    // refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    return res.status(201).json({ message: "succesfully logged out" })
 })
 
 //this route handles all our request for new acess tokens
 //we use refresh tokens to create new access tokens
-userRouter.post('/token', (req, res, next) => {
+userRouter.post('/token', async (req, res, next) => {
+    const token = req.body.token
+    if (!token) return res.status(401)
     try {
-        const refreshToken = req.body.token
-        if (!refreshToken) return res.status(401)
-        if (refreshTokens.includes(refreshToken)) {
-            const user = jwt.verify(refreshToken, process.env.REFRESH_KEY)
-            const newAccessToken = jwt.sign({ username: user.user, id: user.id }, process.env.SECRET_KEY, { expiresIn: "30s" })
-            
+        const userFromToken = jwt.verify(token, process.env.REFRESH_KEY)
+        const user = await User.findById(userFromToken.id)
+        if (user && user.refreshToken === token) {
+            const newAccessToken = jwt.sign({ username: user.username, id: user._id }, process.env.SECRET_KEY, { expiresIn: "30s" })
+
             //sending new access tokens to user if the refresh token validates 
-            return res.json(newAccessToken)
+            return res.json({ newAccesToken: newAccessToken })
         } else {
             return res.status(403).json({ error: "invalid refresh token" })
         }
@@ -51,7 +55,7 @@ userRouter.post('/login', async (req, res, next) => {
     const { password, username } = req.body
 
     const user = await User.findOne({ username })
-    const passwordFound = user === null ? false : await bcrypt.compare(password, user.passwordHash)
+    const passwordFound = user === null ? false : bcrypt.compare(password, user.passwordHash)
     if (!user && !passwordFound) {
         return res.status(401).json({ error: 'either username or password is not matching' })
     }
@@ -63,9 +67,10 @@ userRouter.post('/login', async (req, res, next) => {
     //creating refreshtoken and access token using jwt
     const refreshToken = jwt.sign(userForToken, process.env.REFRESH_KEY);
     const token = jwt.sign(userForToken, process.env.SECRET_KEY, { expiresIn: '30s' })
-    refreshTokens.push(refreshToken)
+    user.refreshToken = refreshToken
+    await user.save()
     //send tokens to user in response body
-    return res.status(201).json({ token, refreshToken, username: user.username })
+    return res.status(201).json({ token, refreshToken, username: user.username, userId : user._id })
 })
 
 //this route is used for registering the user
