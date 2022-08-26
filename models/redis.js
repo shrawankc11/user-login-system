69323c2ff20b37eeceb4ef0b70f71e785bf7940a
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-08-26 12:09:29 +0545

    added reids to project

---
+++ models/redis.js
@@ -0,0 +1,10 @@
+const redis = require("redis");
+
+const client = redis.createClient();
+
+client
+    .connect()
+    .then(() => console.log("connected to redis client"))
+    .catch(() => console.log("error while connecting"));
+
+module.exports = client;
