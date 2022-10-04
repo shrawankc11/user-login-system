b48a8e2e67c5256f4894268fadd05acabb13218b
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-04 15:56:29 +0545

    modularized the repo for middleware
    created a seperate file for redis look up

---
+++ src/middleware/cacheLookUp.js
@@ -0,0 +1,46 @@
+const redisClient = require('../utils/redisClient');
+const bcrypt = require('bcrypt');
+const getToken = require('../utils/tokenGen');
+
+/**
+ * middleware to lookup for user in redis before giving off the controller to main controller
+ * pass controll if not found with username
+ * thorw error if not found with both username and password
+ */
+const cacheLookUp = async (err, req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        const userFromRedis = await redisClient.get(username);
+
+        if (!userFromRedis) {
+            next();
+            return;
+        }
+
+        const user = JSON.parse(userFromRedis);
+
+        const passwordFromRedis = user
+            ? await bcrypt.compare(password, user.passwordHash)
+            : null;
+
+        if (!passwordFromRedis) {
+            return res.status(401).json({ error: 'invalid password!' });
+        }
+        console.log('redis hit got user data from redis');
+
+        const payload = {
+            username: user.username,
+            id: user.id,
+        };
+
+        return res.status(200).json({
+            success: true,
+            ...getToken(payload, 2),
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = cacheLookUp;
--- src/middleware/commonHandlers.js
+++ src/middleware/commonHandlers.js
@@ -4,21 +4,20 @@ TokenExtrator: to extract token passed by user for tokenRequired endpoints
 errorHandler: express error handler for specific errors
 */
 
-
 const errorHandler = (err, req, res, next) => {
-    if (err.name === "TokenExpiredError") {
-        return res.status(401).json({ error: "token expired" })
-    } else if (err.name === "JsonWebTokenError") {
-        return res.status(401).json({ error: "invalid token" })
-    } else if (err.name === "TypeError") {
-        return res.status(401).json({ error: "invalid token" })
+    if (err.name === 'TokenExpiredError') {
+        res.status(401).json({ error: 'token expired' });
+    } else if (err.name === 'JsonWebTokenError') {
+        res.status(401).json({ error: 'invalid token' });
+    } else if (err.name === 'TypeError') {
+        res.status(401).json({ error: 'user error' });
     }
 
-    next(err)
-}
+    next(err);
+};
 
 const unkownEndpoint = (req, res) => {
-    return res.status(400).json({ error: "unknown endpoint" })
-}
+    return res.status(400).json({ error: 'unknown endpoint' });
+};
 
-module.exports = { errorHandler, unkownEndpoint }
+module.exports = { errorHandler, unkownEndpoint };
