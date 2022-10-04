7b9246b6fb87a7b43fd15c7ea3c620b0f9202f16
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-04 19:17:50 +0545

    code cleanup

--- src/middleware/commonHandlers.js
+++ src/middleware/commonHandlers.js
@@ -1,16 +1,15 @@
 /*
 Middleware functions 
-TokenExtrator: to extract token passed by user for tokenRequired endpoints
 errorHandler: express error handler for specific errors
 */
 
 const errorHandler = (err, req, res, next) => {
     if (err.name === 'TokenExpiredError') {
-        res.status(401).json({ error: 'token expired' });
+        res.status(401).json({ error: 'session timeout!' });
     } else if (err.name === 'JsonWebTokenError') {
-        res.status(401).json({ error: 'invalid token' });
+        res.status(401).json({ error: 'invalid token!' });
     } else if (err.name === 'TypeError') {
-        res.status(401).json({ error: 'user error' });
+        res.status(401).json({ error: 'user error!' });
     }
 
     next(err);
